a7f0ad6f6d2eb8f37112fc86551c91f6c37b7414
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-11-26 13:03:31 +0100

    4 TDD cases passed

---
+++ HabitToHelp/ProductApplication/us1_signup_in/index.test.js
@@ -0,0 +1,95 @@
+
+const request = require('supertest');
+const app = require('./app')
+
+
+ 
+
+//validate 
+// empty input, not match pw with pw-con, 
+//pw length 
+
+// get /(index)  (test to working jest)
+describe('GET /', () => {
+    it('should responde to index', (done) => {
+        request(app)
+      .get('/')
+      .send('Signup for an account')
+      .expect(200, done)
+ 
+    })
+})
+
+// POST /signup 
+
+
+describe('POST /signup', () => {
+    it('should sign up with username, name , email and passwrod ', () => {
+        return request(app)
+        .post('/signup')
+        .send({username: 'test', name: 'testName', email: 'test@test.com', password: 'abc123', password_confirm: 'abc123' })
+        .expect(201)
+        .then((res) => {
+            expect(res.body).toEqual(
+              expect.objectContaining({
+                  username: expect.any(String),
+                  name: expect.any(String),
+                  email: expect.any(String),
+                  password: expect.anything(),
+          })
+          );
+      
+    });
+
+    })
+  
+    //check empty input
+    it('should return err, if input is empty', () => {
+        return request(app).post('/signup')
+        .send({username: '', name: '', email: '', password: '', password_confirm: '' })
+        .expect(422);
+
+    });
+
+    //check password and pw_confirm match
+    it('should return err, if pw and pw_confirm does not match', () => {
+        return request(app).post('/signup')
+        .send({password: '123', password_confirm: '456' })
+       // .expect(password).not.toBe(password_confirm)
+        .expect(422)
+        
+
+    });
+
+    //check pw length < 6
+    // it('should return err, if pw is less then 6 character', () => {
+    //     const password = '123';
+    //     return request(app).post('/signup')
+    //     .send({password: '123'})
+        // .expect(422)
+        
+    //     .then((res) => {
+        // .expect(password.length).toBeLessThan(6)
+        
+    
+     //.expect(password).toHaveLength(3)         
+          
+      
+    // });
+    //     .then(() => {
+    //         expect(password.length).toEqual(
+    //         expect.objectContaining({
+                
+    //             password: expect.toBeLessThan(6),
+    //     })
+    //     );
+    
+    // });
+
+
+
+
+
+
+});
+
--- HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
+++ HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 //const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-
+const creatErrors = require('http-errors');
 
 const app = express();
 
@@ -15,5 +15,35 @@ router.get("/",  (req, res) => res.render('welcome'));
 //     name: req.user.name
 // }));
 
+router.post("/signup", (req, res, next) => {
+    const {username, name, email, password, password_confirm} = req.body;
+
+    if (!name || !email || !password || !password_confirm) {
+        return next(creatErrors(422, 'Validation Error'));
+      }
+      if (password != password_confirm) {
+        return next(creatErrors(422, 'Validation Error'));
+      }
+      //check pw length
+      if (password.length < 6) {
+        return next(creatErrors(422, 'Validation Error'));
+    }
+      
+
+
+    //creat new user 
+    const newUser = {
+        username,
+        name,
+        email,
+        password,
+        password_confirm
+    };
+
+    res.status(201).send(newUser);
+
+
+
+})
 
 module.exports = router;
