c7c55d695cd8853a9368c219489de9b548a1a94e
sasaje <sara590x@edu.easj.dk>
2021-11-30 13:54:13 +0100

    signUpSuccess routes added

--- HabitToHelp/ProductApplication/us1_signup_in/app.js
+++ HabitToHelp/ProductApplication/us1_signup_in/app.js
@@ -22,10 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/signUpSuccess', indexRouter);
 app.use('/dashboard', dashboardRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signUpRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
