afcd3d9d4fd8af8d2b88812a8fe7309e5aea963b
sasaje <sara590x@edu.easj.dk>
2021-11-30 13:46:18 +0100

    Testcase 2 updated with view also in code

--- HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
+++ HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
@@ -7,15 +7,15 @@ const router = express.Router();
 router.get("/",  (req, res) => res.render('index'));
 
 //POST success redirect to dashboard or fallback to index
-router.post("/dashboard",  (req, res) => {
+router.post("/",  (req, res) => {
     const {email, password} = req.body;
     const newSignIn = {
         email,
         password        
     };
     console.log(newSignIn);
-    res.status(201).send(newSignIn); //should render to welcome/login page with success msg
- //   res.render('dashboard');
+//  res.status(201).send(newSignIn); //should render to welcome/login page with success msg
+    res.status(201).render('signUpSuccess');
 });
 
 module.exports = router;
--- HabitToHelp/ProductApplication/us1_signup_in/test/index.test.js
+++ HabitToHelp/ProductApplication/us1_signup_in/test/index.test.js
@@ -13,13 +13,13 @@ describe('GET /', () => {
 });
 
 /*TESTCASE 2:*/
-describe('POST /dashboard', () => {
+describe('POST /signUpSuccess', () => {
     it('Should respond with dashboard when sign-in success', function() {
         const testUser = {email: 'test', password: 'abc123'}
         expect(testUser.email).toBeDefined();
         expect(testUser.password).toBeDefined();
         return request(app)
-        .post('/dashboard')
+        .post('/')
         .send(testUser)
         .expect(201)
     })
