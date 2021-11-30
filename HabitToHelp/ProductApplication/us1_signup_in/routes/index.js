95e14d7d7fadb10161ccf125b20a79b220174efe
sasaje <sara590x@edu.easj.dk>
2021-11-30 11:53:08 +0100

    Code for testcase 2 added

--- HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
+++ HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
@@ -3,11 +3,19 @@ const express = require('express');
 const router = express.Router();
 //const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-
 //GET routes index page
 router.get("/",  (req, res) => res.render('index'));
 
-
-
+//POST success redirect to dashboard or fallback to index
+router.post("/dashboard",  (req, res) => {
+    const {username, password} = req.body;
+    const newSignIn = {
+        username,
+        password        
+    };
+    console.log(newSignIn);
+    res.status(201).send(newSignIn); //should render to welcome/login page with success msg
+ //   res.render('dashboard');
+});
 
 module.exports = router;
