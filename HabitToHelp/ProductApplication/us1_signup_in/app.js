3b826b1c4bd821b52c913bcbd080233fa3fec7e3
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-11-30 17:48:23 +0100

    change router (app.use) to signUpRouter

--- HabitToHelp/ProductApplication/us1_signup_in/app.js
+++ HabitToHelp/ProductApplication/us1_signup_in/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/signUpSuccess', indexRouter);
+app.use('/signUpSuccess', signUpRouter);
 app.use('/dashboard', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signUpRouter);
