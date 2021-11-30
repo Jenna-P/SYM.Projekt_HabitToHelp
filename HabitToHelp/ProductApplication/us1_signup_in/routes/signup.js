aa55dbeef4d5773271086267840418d7b41a2a04
sasaje <sara590x@edu.easj.dk>
2021-11-30 12:56:54 +0100

    changes made signup added

--- HabitToHelp/ProductApplication/us1_signup_in/routes/signup.js
+++ HabitToHelp/ProductApplication/us1_signup_in/routes/signup.js
@@ -6,7 +6,6 @@ const router = express.Router();
 //GET routes sign up page
 router.get("/",  (req, res) => res.render('sign-up'));
 
-
 router.post("/", (req, res, next) => {
     const {username, name, email, password, password_confirm} = req.body;
     let errors = [];
@@ -14,7 +13,6 @@ router.post("/", (req, res, next) => {
      if (!name || !email || !password || !password_confirm) {
         errors.push({ msg: 'Please enter all fields' });
         res.send('err');  //just to check that is working
-        
       }
     //check pw match
       if (password != password_confirm) {
@@ -39,21 +37,18 @@ router.post("/", (req, res, next) => {
         console.log('err');
        
       } else {
-           
-      
-    //creat new user 
-    const newUser = {
-        username,
-        name,
-        email,
-        password
-        
-    };
-     console.log(newUser);
-    res.status(201).send(newUser); //should render to welcome/login page with success msg
-    //res.status(201).render('index');
-}
-
+        //create new user 
+        const newUser = {
+            username,
+            name,
+            email,
+            password
+            
+        };
+        console.log(newUser);
+        res.status(201).send(newUser); //should render to welcome/login page with success msg
+        //res.status(201).render('index');
+      }
 })
 
 module.exports = router;
