7c79573ad080883a1809cd72e5448468b558a59d
sasaje <sara590x@edu.easj.dk>
2021-11-30 11:34:00 +0100

    testcase 2 pseudo code added

--- HabitToHelp/ProductApplication/us1_signup_in/test/index.test.js
+++ HabitToHelp/ProductApplication/us1_signup_in/test/index.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
+/*TESTCASE 1:*/
 // get /(index)  (test to working jest)
 describe('GET /', () => {
     it('should responde to index', (done) => {
@@ -9,5 +10,18 @@ describe('GET /', () => {
       .send('index')
       .expect(200, done)
     })
-})
+});
+
+/*TESTCASE 2:*/
+describe('GET /', () => {
+    it('Should respond with dashboard when sign-in success', (done) => {
+
+    });
+});
+
+describe('GET /', () => {
+    it('', (done) => {
+
+    });
+});
 
