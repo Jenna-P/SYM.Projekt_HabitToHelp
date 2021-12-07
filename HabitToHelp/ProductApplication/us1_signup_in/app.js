d59b0bb02bf20172cfeeb9c895a616311b941268
sasaje <sara590x@edu.easj.dk>
2021-12-07 10:36:48 +0100

    setup app.js added

--- HabitToHelp/ProductApplication/us1_signup_in/app.js
+++ HabitToHelp/ProductApplication/us1_signup_in/app.js
@@ -21,6 +21,37 @@ var MongoDBStore = require('connect-mongodb-session')(session);
 
 var app = express();
 
+//Mongo DB Atlas
+const db = process.env.DBURL;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+//set session store - model/schema  
+var store = new MongoDBStore({
+  uri: db,
+  collection: 'mySession'
+});
+
+//save session to DB - sets session for user on db.
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  // Boilerplate options, see:
+  // * https://www.npmjs.com/package/express-session#resave
+  // * https://www.npmjs.com/package/express-session#saveuninitialized
+  resave: true,
+  saveUninitialized: true
+}));
+
+//passport config
+require('./config/passport')(passport);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -30,6 +61,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables  //put msg to flash and get them here
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+}); 
 
 app.use('/', indexRouter);
 app.use('/dashboard', dashboardRouter);
@@ -43,20 +89,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+// render the error page
+res.status(err.status || 500);
   res.render('error');
 });
-
-//Mongo DB Atlas
-
-var dburl = process.env.DBURL;
-
-
 
 
 
