3e80b49623bac0619c530ac70a91afd50b453fdc
sasaje <sara590x@edu.easj.dk>
2021-12-07 11:51:57 +0100

    auth to index.js added

--- HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
+++ HabitToHelp/ProductApplication/us1_signup_in/routes/index.js
@@ -1,21 +1,20 @@
 const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
+const passport = require('passport'); //for auth
 //const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 //GET routes index page
 router.get("/",  (req, res) => res.render('index'));
 
-
 //POST success redirect to dashboard or fallback to index
-router.post("/",  (req, res) => {
-    const {email, password} = req.body;
-    const newSignIn = {
-        email,
-        password        
-    };
-    console.log(newSignIn);
-    res.status(201).redirect('/dashboard'); //success scenario. 
+//Login handle 
+router.post('/', (req, res, next) => {
+    passport.authenticate('local', {  //using local strategy 
+      successRedirect: '/dashboard',
+      failureRedirect: '/',  //fail  : go back to login page
+      failureFlash: true
+    })(req, res, next);    
 });
 
 module.exports = router;
