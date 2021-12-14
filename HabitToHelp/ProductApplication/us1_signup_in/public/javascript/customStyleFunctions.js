e60dff8c187d789bc453096e109708dfb1d7e894
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2021-12-14 10:50:40 +0100

    added id attr:active

--- HabitToHelp/ProductApplication/us1_signup_in/public/javascript/customStyleFunctions.js
+++ HabitToHelp/ProductApplication/us1_signup_in/public/javascript/customStyleFunctions.js
@@ -1,1 +1,19 @@
 //clientside custom js functions here
+
+let  currentHabit = document.querySelector(".testHabit");
+
+
+function addActiveId() {
+    if(currentHabit.id =="") {
+        currentHabit.setAttribute("id","active");
+      }
+      else{
+        currentHabit.removeAttribute("id");
+        currentHabit.setAttribute("id", "");
+      }
+}
+
+
+
+//if currentHabit has id active
+//show currentHabit prop in EJS
