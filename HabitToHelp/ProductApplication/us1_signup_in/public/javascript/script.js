41925d5ca6914f5ba00f5e93a7bf9e6201733cda
sasaje <sara590x@edu.easj.dk>
2021-12-14 14:26:43 +0100

    test function removed

--- HabitToHelp/ProductApplication/us1_signup_in/public/javascript/script.js
+++ HabitToHelp/ProductApplication/us1_signup_in/public/javascript/script.js
@@ -1,20 +1,5 @@
 //clientside custom js functions here
 
-let  currentHabit = document.querySelector(".testHabit");
-
-
-function addActiveId() {
-    if(currentHabit.id =="") {
-        currentHabit.setAttribute("id","active");
-      }
-      else{
-        currentHabit.removeAttribute("id");
-        currentHabit.setAttribute("id", "");
-      }
-}
-
-//if currentHabit has id active
-//show currentHabit prop in EJS
 
 /* Modal JS - create new habit*/
   // Get the modal
