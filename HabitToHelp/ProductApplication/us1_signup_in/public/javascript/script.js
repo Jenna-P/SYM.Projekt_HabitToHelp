3adc8b176e186b02d534cbfb52076b97f743a6a2
sasaje <sara590x@edu.easj.dk>
2021-12-23 14:38:06 +0100

    dashboard updated with edit modal and styling

--- HabitToHelp/ProductApplication/us1_signup_in/public/javascript/script.js
+++ HabitToHelp/ProductApplication/us1_signup_in/public/javascript/script.js
@@ -30,24 +30,52 @@
 
   //send target habit value with GET method
   const items = document.querySelectorAll('li, form.iconButton');
-items.forEach(item => {
-	item.addEventListener('click',(e)=>{  
-    let targetHabit = e.target.innerText;
-    //let targetobjId = e
-		console.log(targetHabit);
-    fetch('/dashboard/'+targetHabit , { method:'GET', data: {habitName: targetHabit}})
-  .then(function (response) {
-    if (response.ok) {
-      console.log('clicked target habit and send value')
-      return
-    }
-    throw new Error('Request failed.')
+    items.forEach(item => {
+      item.addEventListener('click',(e)=>{  
+        let targetHabit = e.target.innerText;
+        //let targetobjId = e
+        console.log(targetHabit);
+        fetch('/dashboard/'+targetHabit , { method:'GET', data: {habitName: targetHabit}})
+      .then(function (response) {
+        if (response.ok) {
+          console.log('clicked target habit and send value')
+          return
+        }
+        throw new Error('Request failed.')
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    })
   })
-  .catch(function (error) {
-    console.log(error)
- })
-   
-    
-	}
-	)
-})
+
+
+  /******** Edit habit modal event ***********/
+  // Get the modal
+  var modalEdit = document.getElementById("myModalEdit");
+
+  // Get the button that opens the modal
+  var btnEdit = document.querySelector(".editHabitButton");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("closeEdit")[0];
+
+  // When the user clicks on the button, open the modal
+  btnEdit.onclick = function() {
+    modalEdit.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modalEdit.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modalEdit) {
+      modalEdit.style.display = "none";
+    }
+  }
+
+  /* For edit habit modal information from chosen habit */
+  
